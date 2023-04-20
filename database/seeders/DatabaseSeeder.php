33f765cc8ed0569fefd1ab30da5354a5bdf1e929
Tyler Holhubner <tholhubner@gmail.com>
2023-04-20 01:29:43 -0400

    personalized the main test user to something more memorable

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -15,8 +15,8 @@ class DatabaseSeeder extends Seeder
         \App\Models\User::factory(10)->create();
 
         \App\Models\User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
+            'name' => 'Tyler Holhubner',
+            'email' => 'tyler@test.com',
         ]);
 
         \App\Models\PetType::factory(10)->create();
