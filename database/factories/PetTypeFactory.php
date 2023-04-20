c629c88ea0c50e38edeb106f31d39ba368d050e0
Tyler Holhubner <tholhubner@gmail.com>
2023-04-20 01:26:33 -0400

    removed function from copied UserFactory that was not needed in PetTypeFactory

--- database/factories/PetTypeFactory.php
+++ database/factories/PetTypeFactory.php
@@ -21,14 +21,4 @@ class PetTypeFactory extends Factory
             'name' => fake()->name(),
         ];
     }
-
-    /**
-     * Indicate that the model's email address should be unverified.
-     */
-    public function unverified(): static
-    {
-        return $this->state(fn (array $attributes) => [
-            'email_verified_at' => null,
-        ]);
-    }
 }
